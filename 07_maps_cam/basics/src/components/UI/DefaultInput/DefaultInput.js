f5955814ed4d946c2969eb75020dd75fe8639afd
Carlos Bahnlink <carlware.sys@gmail.com>
2019-04-26 12:06:09 -0500

    maps

---
+++ 07_maps_cam/basics/src/components/UI/DefaultInput/DefaultInput.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { TextInput, StyleSheet } from "react-native";
+
+const defaultInput = props => (
+    <TextInput
+        underlineColorAndroid="transparent"
+        {...props}
+        style={[styles.input, props.style, !props.valid && props.touched ? styles.invalid : null]}
+    />
+);
+
+const styles = StyleSheet.create({
+  input: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#eee",
+    padding: 5,
+    marginTop: 8,
+    marginBottom: 8
+  }
+});
+
+export default defaultInput;
